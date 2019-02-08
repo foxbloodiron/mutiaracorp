91f0ca1ba991d8d0f87b5614bf3d23da883e319e
David Ridwan <ddavidridwan@gmail.com>
2019-02-08 02:45:43 -0800

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,36 +6,38 @@
 <body>
     <div class="auth">
             <div class="auth-container">
-                <div class="card">
+                <div class="card login-section">
                     <header class="auth-header">
                         <h1 class="auth-title">
                           <div class="logo">
-                              <span class="l l1"></span>
+                          <img src="{{asset('assets/img/cv-mutiaraberlian-icon.png')}}" height="45px" width="45px" class="login-logo">
+                              <!-- <span class="l l1"></span>
                               <span class="l l2"></span>
                               <span class="l l3"></span>
                               <span class="l l4"></span>
-                              <span class="l l5"></span>
+                              <span class="l l5"></span> -->
                           </div class="login-text">Mutiara Berlian</h1>
                     </header>
                     <div class="auth-content">
-                        <p class="text-center">LOGIN TO CONTINUE</p>
                         <form method="POST" action="{{ url('login') }}">
                         {{ csrf_field() }}
                             <div class="form-group">
-                                <label for="email">Username / E-mail</label>
-                                <input type="text" class="form-control underlined" name="email" id="email" placeholder="Your Username or E-mail address" required autofocus=""> </div>
+                                <label for="email">Username</label>
+                                <input type="text" class="form-control login-input" placeholder="Username" required autofocus=""> </div>
+                                <br>
                             <div class="form-group">
                                 <label for="password">Password</label>
-                                <input type="password" class="form-control underlined" name="password" id="password" placeholder="Your password" required> </div>
+                                <input type="password" class="form-control login-input" name="password" id="password" placeholder="Password" required> </div>
+                                <br>
                             <div class="form-group">
                                 <label for="remember">
-                                    <input class="checkbox" id="remember" name="remember" type="checkbox">
+                                    <input class="checkbox check-remember" id="remember" name="remember" type="checkbox">
                                     <span>Remember me</span>
                                 </label>
                                 <!-- <a href="reset.html" class="forgot-btn pull-right">Forgot password?</a> -->
                             </div>
                             <div class="form-group">
-                                <button type="submit" class="btn btn-block btn-primary">Login</button>
+                                <button type="submit" class="btn btn-block btn-primary btn-login">Login</button>
                             </div>
                             <!-- <div class="form-group">
                                 <p class="text-muted text-center">Do not have an account?
